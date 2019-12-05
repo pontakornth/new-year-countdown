f35c20beb43e582f2ee209d5c55282cee754d324
pontakornth <pontakorn61@gmail.com>
2019-12-05 18:32:05 +0700

    Change tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            display: ['Chonburi','sans-serif'],
+            body: ['K2D','sans-serif']
+        }
+    },
+    variants: {},
+    plugins: [],
+  }

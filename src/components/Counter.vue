be101b3546d11f3adf709587fd58f4fd2a42a8ca
pontakornth <pontakorn61@gmail.com>
2020-12-24 11:53:21 +0700

    refactor: Make postcss clear

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -67,7 +67,7 @@ export default class HelloWorld extends Vue {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="postcss" scoped>
 @keyframes scale {
   0% {
     transform: scale(1);
